fd98833248ce303158613a86607ace58b71b4d11
Eugene <eleonkov@gmail.com>
2020-10-20 16:51:10 +0300

    add

--- util/index.js
+++ util/index.js
@@ -6,22 +6,42 @@ const isFresh = (post, tstmp) => {
     return post.node.taken_at_timestamp > tstmp;
 }
 
+const getGridPost = (sortedContent) => {
+
+    return function (typename) {
+        const grapContent = sortedContent.filter(elem => elem.typename === typename);
+
+        return grapContent.map((post) => (`
+            <div class="grid-item ${post.typename}">
+                <span>❤ ${post.edge_liked_by.count}</span>
+                <a target="_blank" href="https://www.instagram.com/p/${post.shortcode}/">
+                    <img src=${post.thumbnail_src} />
+                </a>
+            </div>
+        `)).join('');
+    }
+}
+
 const getView = (content) => {
-	const sortedContent = content.sort((a, b) => b.edge_liked_by.count -  a.edge_liked_by.count);
-	
-    const li = sortedContent.map((post) => (`
-        <div class="grid-item ${post.typename}">
-			<span>❤ ${post.edge_liked_by.count}</span>
-            <a target="_blank" href="https://www.instagram.com/p/${post.shortcode}/">
-                <img src=${post.thumbnail_src} />
-            </a>
-        </div>
-    `));
+    const sortedContent = content.sort((a, b) => b.edge_liked_by.count - a.edge_liked_by.count);
+
+    const getPosts = getGridPost(sortedContent);
+
+    const graphVideoList = getPosts('GraphVideo');
+    const GraphSidecarList = getPosts('GraphSidecar');
+    const graphImageList = getPosts('GraphImage');
 
     return `<html>
         <head><link rel="stylesheet" href="./styles.css">
 		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet"></head>
-        <body><div class="grid-container">${li.join('')}</div></body>
+        <body>
+            <h1 style="font-family: 'Montserrat';">Graph Video</h1>
+            <div class="grid-container">${graphVideoList}</div>
+            <h1 style="font-family: 'Montserrat';">Graph Sidecar</h1>
+            <div class="grid-container">${GraphSidecarList}</div>
+            <h1 style="font-family: 'Montserrat';">Graph Image</h1>
+            <div class="grid-container">${graphImageList}</div>
+        </body>
     </html>`;
 }
 
