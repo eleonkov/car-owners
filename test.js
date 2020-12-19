e64f49e3deffb3716d495b58033daf3abfcb003d
Igor <C:\Users\User\AppData\Roaming\The Bat!>
2020-12-19 23:08:40 +0300

    upd

--- test.js
+++ test.js
@@ -1,39 +1,21 @@
-const fetch = require('node-fetch');
-var ProxyAgent = require('http-proxy-agent');
+let Instagram = require('./instagram');
 
-const agent = new ProxyAgent('http://89.187.177.103:80');
+const igLogin = process.argv[2];
+const igPassword = process.argv[3];
 
-const buildProxy = (url) => {
-    return {
-        agent
-    }
-}
+Instagram = new Instagram();
 
-const getResource = async (url) => {
-    const proxyData = buildProxy(url);
 
-    try {
-        const res = await fetch(url, {
-            agent: agent,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        });
-
-        if (!res.ok) {
-            console.log(res)
-        }
-
-        return await res.json();
-    } catch (e) {
-        console.log(e.message)
-    }
-};
-
-const app = async () => {
-    const data = await getResource('https://api.ipify.org/?format=json');
-
-    console.log(data);
-}
-
-app();
+Instagram.getCsrfToken().then((csrf) =>
+{
+  Instagram.csrfToken = csrf;
+}).then(() =>
+{
+  return Instagram.auth(igLogin, igPassword).then(sessionId =>
+  {
+    Instagram.sessionId = sessionId
+ 
+    console.log(Instagram.sessionId);
+    console.log(Instagram.csrfToken);
+  })
+}).catch(console.error);
