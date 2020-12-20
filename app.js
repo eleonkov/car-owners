62226198a61aff5a32ddf5d281c4e0ace95e6e15
Igor <C:\Users\User\AppData\Roaming\The Bat!>
2020-12-20 13:36:38 +0300

    upd

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ require('dotenv').config({
     path: __dirname + '/.env'
 });
 
-Instagram = new Instagram();
+// Instagram = new Instagram();
 
-Instagram.sessionId = "10384331809%3ACIK5NgOKvRKi3K%3A20";
-Instagram.csrfToken = "5ej7GNlPNooRPg5T55nFS5rvnNDSFero";
+// Instagram.sessionId = "10384331809%3ACIK5NgOKvRKi3K%3A20";
+// Instagram.csrfToken = "5ej7GNlPNooRPg5T55nFS5rvnNDSFero";
 
 const carAppInit = async () => {
     const CAR = process.env.CAR || 'nissan';
@@ -25,12 +25,16 @@ const carAppInit = async () => {
     const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
 
 
-    const getResource = async (owner) => {
-        const ownerData = await Instagram.getUserDataByUsername(owner);
+    const getResource = async (url) => {
+        //const ownerData = await Instagram.getUserDataByUsername(owner);
+        const res = await fetch('https://www.instagram.com/' + url);
 
-        if (!ownerData) return null;
+        if (!res.ok) return null;
+        // if (!ownerData) return null;
+        
+        return await res.json();
 
-        return ownerData;
+        // return ownerData;
     };
 
     const getContent = (posts) => {
@@ -57,7 +61,7 @@ const carAppInit = async () => {
         let tmpShortcodeList = [];
 
         for (let owner of db[CAR].owners) {
-            const res = await getResource(owner);
+            const res = await getResource(`${owner}/?__a=1`);
             if (res) {
                 const shortcodeList = getContent(res.graphql.user.edge_owner_to_timeline_media.edges);
 
