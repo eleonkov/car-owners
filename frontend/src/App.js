be02dcecef58c8aed54555f9eb57d83c1a9e2827
AlexLeonkov <57963116+AlexLeonkov@users.noreply.github.com>
2021-12-30 23:25:44 +0300

    upd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import React, { useEffect, useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import Iframe from "react-iframe";
+import "react-tabs/style/react-tabs.css";
 
 const App = () => {
   const [state, setState] = useState([]);
 
   useEffect(() => {
     if (window._sharedData) {
-      setState(window._sharedData)
+      setState(window._sharedData);
     }
   }, []);
 
   const renderContentCard = (type, sortBy) => {
-    const sharedContent = state.sort((a, b) => b.insight[sortBy] - a.insight[sortBy])
-      .filter(edge => edge.typename === type);
+    const sharedContent = state
+      .sort((a, b) => b.insight[sortBy] - a.insight[sortBy])
+      .filter((edge) => edge.typename === type);
 
     return (
       <div className="grid-container">
-        {sharedContent.map(edge => (
+        {sharedContent.map((edge) => (
           <div key={edge.shortcode} class={`grid-item ${edge.typename}`}>
-            <span>Comments: {edge.insight.comments}<br/>Likes: {edge.insight.likes}</span>
-            <a target="_blank" href={`https://www.instagram.com/p/${edge.shortcode}/`}>
-              <img src={edge.thumbnail} />
-            </a>
+            <iframe
+              src={`https://www.instagram.com/p/${edge.shortcode}/embed`}
+              id={edge.shortcode}
+              key={edge.shortcode}
+              width="100%"
+              height="400px"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
           </div>
         ))}
       </div>
-    )
+    );
   };
 
   return (
@@ -54,7 +62,7 @@ const App = () => {
         {renderContentCard("GraphImage", "likes")}
       </TabPanel>
     </Tabs>
-  )
-}
+  );
+};
 
 export default App;
